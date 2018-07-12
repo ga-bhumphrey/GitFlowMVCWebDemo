3f3180e20aea428dd5df069a284232e8b35d24d3
Humpy <bhumphrey@gaig.com>
2018-07-12 15:29:57 -0400

    Merge branch 'master' into develop
    Added a repositoy view and controller and a list.

---
+++ MVCWebDemo/Controllers/RepositoriesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCWebDemo.ViewModels;
+
+namespace MVCWebDemo.Controllers
+{
+    public class RepositoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var viewModel = new RepositoriesViewModel();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ MVCWebDemo/ViewModels/RepositoriesViewModel.cs
@@ -0,0 +1,29 @@
+﻿using MVCWebDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebDemo.ViewModels
+{
+    public class RepositoriesViewModel
+    {
+        public List<RepositoryModel> RepositoryList = new List<RepositoryModel>();
+
+        public RepositoriesViewModel()
+        {
+            AddRepos();
+        }
+
+        private void AddRepos()
+        {
+            var repo = new RepositoryModel
+            {
+                Name = "GitFlowDemo",
+                DateCreated = DateTime.Now
+            };
+
+            RepositoryList.Add(repo);
+        }
+    }
+}
